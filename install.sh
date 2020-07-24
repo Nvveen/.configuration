56682763660b35ef39832dbb5d5de2daa4e3e76f
Neal van Veen <nvanveen@magnius.com>
2020-07-24 18:16:55 +0200

    add powerlevel10k

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ cp vim/vimrc $HOME/.vimrc
 y | sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 ln -s $PWD/ohmyzsh/custom/plugins/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 ln -s $PWD/ohmyzsh/custom/plugins/zsh-syntax-highlighting $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+ln -s $PWD/ohmyzsh/custom/themes/powerlevel10k $HOME/.oh-my-zsh/custom/themes/powerlevel10k
 
 cp zsh/zshrc ~/.zshrc
 cp zsh/zprofile ~/.zprofile
