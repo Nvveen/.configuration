4a62079be90547c48c224219c90a0657e4c5431c
Neal van Veen <nvanveen@magnius.com>
2020-07-24 17:53:55 +0200

    Add zsh plugins

--- install.sh
+++ install.sh
@@ -6,6 +6,8 @@ cp .tmux/.tmux.conf.local $HOME/.tmux.conf.local
 cp vim/vimrc $HOME/.vimrc
 
 y | sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+ln -s zsh/custom/plugins/zsh-autosuggestions $HOME/.oh-my-zsh/custom/pugins/zsh-autosuggestions
+ln -s zsh/custom/plugins/zsh-syntax-highlighting $HOME/.oh-my-zsh/custom/pugins/zsh-syntax-highlighting
 
 cp zsh/zshrc ~/.zshrc
 cp zsh/zprofile ~/.zprofile
