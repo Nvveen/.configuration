168e3ae694a52f7a857e932bbe61fb0743d3e3ad
Neal van Veen <nvanveen@magnius.com>
2020-07-24 18:04:17 +0200

    fix more

--- install.sh
+++ install.sh
@@ -6,8 +6,8 @@ cp .tmux/.tmux.conf.local $HOME/.tmux.conf.local
 cp vim/vimrc $HOME/.vimrc
 
 y | sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-ln -s $PWD/zsh/custom/plugins/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-ln -s $PWD/zsh/custom/plugins/zsh-syntax-highlighting $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+ln -s $PWD/ohmyzsh/custom/plugins/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+ln -s $PWD/ohmyzsh/custom/plugins/zsh-syntax-highlighting $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
 cp zsh/zshrc ~/.zshrc
 cp zsh/zprofile ~/.zprofile
